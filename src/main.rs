da0c02559f5640fcaab0f2a4386e377045be8726
Rocco Antoniou <roccoantoniou@gmail.com>
2024-12-04 16:50:06 -0700

    organization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-mod day1;
-mod day2;
-mod day3;
-mod day4;
+mod solutions;
 
 fn print_header(day: i32) {
     print!("---------- Day {} -----------\n", day);
@@ -9,14 +6,14 @@ fn print_header(day: i32) {
 
 fn main() {
     print_header(1);
-    day1::day1();
+    solutions::day1::day1();
 
     print_header(2);
-    day2::day2();
+    solutions::day2::day2();
 
     print_header(3);
-    day3::day3();
+    solutions::day3::day3();
 
     print_header(4);
-    day4::day4();
+    solutions::day4::day4();
 }
